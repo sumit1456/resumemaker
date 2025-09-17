28a0be0e48b77fc286ebfcd7155f9f9d4c383692
sumit1456 <sumithatekar067@gmail.com>
2025-09-17 18:13:20 +0530

    Update GlobalExceptionHandler.java

--- src/main/java/com/app/resumemaker/exception/GlobalExceptionHandler.java
+++ src/main/java/com/app/resumemaker/exception/GlobalExceptionHandler.java
@@ -3,11 +3,14 @@ package com.app.resumemaker.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-	
+	 @ExceptionHandler(UserNotFound.class)
+	 @ResponseStatus(HttpStatus.NOT_FOUND)
 	public ResponseEntity<String> userNotFound(UserNotFound unf){
 		return ResponseEntity.status(HttpStatus.NOT_FOUND)
 				.body(unf.getMessage());
